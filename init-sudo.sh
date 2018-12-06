86fb2ad18fc54e34c5cc9fca35467d23406eecf3
XiangWangtao <xwt2101239@gmail.com>
2018-12-06 12:31:07 +0800

    test

--- init-sudo.sh
+++ init-sudo.sh
@@ -12,15 +12,16 @@ mkdir -p ~/.vim/colors
 cp ~/molokai.vim ~/.vim/colors/
 vim +PluginInstall +qall
 
+sudo apt install tmux
 git clone https://github.com/gpakosz/.tmux.git
 ln -s -f .tmux/.tmux.conf
-cp .tmux/.tmux.conf.local
-
+cp .tmux/.tmux.conf.local .
 
 sudo apt install build-essential cmake python3-dev
 # youcompleteme compilation
 cd ~/.vim/bundle/YouCompleteMe
-python3 install.py --clang-completer
+pyth
+cd ..on3 install.py --clang-completer
 
 
 # always at end
